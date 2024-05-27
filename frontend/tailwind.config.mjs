ec30073c14e70d0f82e55c55c43986d1349356f4
Araozu <fernando@araozu.dev>
2024-05-27 15:42:30 -0500

    Add daisyui. Cleanup

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,7 +1,14 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
-    plugins: [],
+    plugins: [
+        daisyui,
+    ],
+    daisyui: {
+        themes: ["black"],
+    },
 };
